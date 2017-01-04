78968d5112f490e74f5f9ae3900e829a219aa007
milos92100@gmail.com <milos92100@gmail.com>
2017-01-04 10:52:46 +0100

    Ok

--- src/ChatskyProfiler/Persistence/DatabaseManager.php
+++ src/ChatskyProfiler/Persistence/DatabaseManager.php
@@ -1,14 +1,12 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: milos
- * Date: 22.12.16.
- * Time: 16.48
- */
-
 namespace ChatskyProfiler\Persistence;
 
 
+/**
+ * Class DatabaseManager
+ *
+ * @package ChatskyProfiler\Persistence
+ */
 class DatabaseManager
 {
 
